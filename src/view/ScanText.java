e1b7552e4be7f23fa235398416f7f38286262188
awg.1590 <awg.1590@gmail.com>
2017-11-24 22:40:09 -0300

    Actualizacion de codigo, finalizacion de el panel de vista miniatura

--- src/view/ScanText.java
+++ src/view/ScanText.java
@@ -5,6 +5,14 @@
  */
 package view;
 
+import clasespersonalizadas.PanelImagenesMiniaturas;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author alan
@@ -16,6 +24,16 @@ public class ScanText extends javax.swing.JFrame {
      */
     public ScanText() {
         initComponents();
+        PanelImagenesMiniaturas lbl = new PanelImagenesMiniaturas("1","/home/alan/NetBeansProjects/LectorOCR/src/tmp/10");
+        lbl.setSize(200, 200);
+        PanelImagenesMiniaturas lbl2;
+        lbl2 = new PanelImagenesMiniaturas("2","/home/alan/NetBeansProjects/LectorOCR/src/tmp/11");
+        lbl.setToolTipText("1");
+        lbl2.setToolTipText("2");
+     
+        jPanelVistaMiniatura.add(lbl2);
+        jPanelVistaMiniatura.add(lbl);
+
     }
 
     /**
@@ -28,44 +46,61 @@ public class ScanText extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanelPrincipal = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jPanelAreaTrabajo = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jPanelVistaMiniatura = new javax.swing.JPanel();
         jTabbedPaneTrabajo = new javax.swing.JTabbedPane();
         jPanelPalabrasClaves = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        jPanelSectorBusqueda = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        jPanelListaPalabras = new javax.swing.JPanel();
+        jScrollPanePalabrasClaves = new javax.swing.JScrollPane();
+        jListPalabrasClaves = new javax.swing.JList<>();
+        jButtonProcesar = new javax.swing.JButton();
+        jMenuBarPrincipal = new javax.swing.JMenuBar();
+        jMenuArchivo = new javax.swing.JMenu();
+        jMenuHojas = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanelPrincipal.setLayout(new java.awt.BorderLayout());
 
-        jPanel1.setLayout(new java.awt.BorderLayout());
+        jPanelAreaTrabajo.setLayout(new java.awt.BorderLayout());
 
-        jLabel1.setText("jLabel1");
-        jPanel1.add(jLabel1, java.awt.BorderLayout.CENTER);
+        jPanelVistaMiniatura.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        java.awt.FlowLayout flowLayout1 = new java.awt.FlowLayout();
+        flowLayout1.setAlignOnBaseline(true);
+        jPanelVistaMiniatura.setLayout(flowLayout1);
+        jScrollPane1.setViewportView(jPanelVistaMiniatura);
 
-        jPanelPrincipal.add(jPanel1, java.awt.BorderLayout.CENTER);
+        jPanelAreaTrabajo.add(jScrollPane1, java.awt.BorderLayout.SOUTH);
+
+        jPanelPrincipal.add(jPanelAreaTrabajo, java.awt.BorderLayout.CENTER);
 
         jPanelPalabrasClaves.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanelPalabrasClaves.setLayout(new java.awt.BorderLayout());
 
-        jPanel2.setLayout(new java.awt.BorderLayout());
+        jPanelSectorBusqueda.setLayout(new javax.swing.BoxLayout(jPanelSectorBusqueda, javax.swing.BoxLayout.LINE_AXIS));
 
-        jLabel2.setText("jLabel2");
-        jPanel2.add(jLabel2, java.awt.BorderLayout.CENTER);
+        jLabel2.setText("Buscar palabra");
+        jPanelSectorBusqueda.add(jLabel2);
 
-        jTextField1.setText("jTextField1");
-        jPanel2.add(jTextField1, java.awt.BorderLayout.LINE_END);
+        jTextField1.setColumns(20);
+        jPanelSectorBusqueda.add(jTextField1);
 
-        jButton1.setText("jButton1");
-        jPanel2.add(jButton1, java.awt.BorderLayout.PAGE_END);
+        jPanelPalabrasClaves.add(jPanelSectorBusqueda, java.awt.BorderLayout.PAGE_START);
 
-        jPanelPalabrasClaves.add(jPanel2, java.awt.BorderLayout.PAGE_START);
+        jPanelListaPalabras.setLayout(new java.awt.BorderLayout());
+
+        jScrollPanePalabrasClaves.setViewportView(jListPalabrasClaves);
+
+        jPanelListaPalabras.add(jScrollPanePalabrasClaves, java.awt.BorderLayout.CENTER);
+
+        jButtonProcesar.setText("Procesar");
+        jPanelListaPalabras.add(jButtonProcesar, java.awt.BorderLayout.PAGE_END);
+
+        jPanelPalabrasClaves.add(jPanelListaPalabras, java.awt.BorderLayout.CENTER);
 
         jTabbedPaneTrabajo.addTab("Palabras", jPanelPalabrasClaves);
 
@@ -73,16 +108,27 @@ public class ScanText extends javax.swing.JFrame {
 
         getContentPane().add(jPanelPrincipal, java.awt.BorderLayout.CENTER);
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
+        jMenuBarPrincipal.setToolTipText("");
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+        jMenuArchivo.setText("File");
+        jMenuArchivo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenuArchivoMouseClicked(evt);
+            }
+        });
+        jMenuBarPrincipal.add(jMenuArchivo);
 
-        setJMenuBar(jMenuBar1);
+        jMenuHojas.setText("Edit");
+        jMenuBarPrincipal.add(jMenuHojas);
+
+        setJMenuBar(jMenuBarPrincipal);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuArchivoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuArchivoMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuArchivoMouseClicked
 
     /**
      * @param args the command line arguments
@@ -120,16 +166,20 @@ public class ScanText extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButtonProcesar;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JList<String> jListPalabrasClaves;
+    private javax.swing.JMenu jMenuArchivo;
+    private javax.swing.JMenuBar jMenuBarPrincipal;
+    private javax.swing.JMenu jMenuHojas;
+    private javax.swing.JPanel jPanelAreaTrabajo;
+    private javax.swing.JPanel jPanelListaPalabras;
     private javax.swing.JPanel jPanelPalabrasClaves;
     private javax.swing.JPanel jPanelPrincipal;
+    private javax.swing.JPanel jPanelSectorBusqueda;
+    private javax.swing.JPanel jPanelVistaMiniatura;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPanePalabrasClaves;
     private javax.swing.JTabbedPane jTabbedPaneTrabajo;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
